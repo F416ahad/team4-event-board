af905824afea564ed8d3d472fb3cbe75fd3500e6
Huy Le <HuyLe32006@gmail.com>
2026-04-16 00:02:17 -0400

    feat: create createEvent method in repo

---
+++ src/comment/InMemoryCommentRepository.ts
@@ -0,0 +1,29 @@
+import { randomUUID } from "node:crypto"; // for generating unique IDs
+import { Err, Ok, type Result } from "../lib/result";
+import type { Event, RSVP, RSVPStatus } from "./Rsvp.ts";
+import type { RSVPRepository } from "./RsvpRepository.ts";
+
+class InMemoryRsvpRepository implements RSVPRepository {
+    private events: Event[] = [];
+
+    async createEvent(title: string, createdByUserId: string): Promise<Result<Event, Error>> {
+        try {
+            const event: Event = {
+                id: randomUUID(),
+                title,
+                rsvps: [],
+                status: "active",
+                date: new Date().toISOString(),
+                createdByUserId,
+            };
+            this.events.push(event);
+
+            return Ok(event);
+        } 
+        catch 
+        {
+            return Err(new Error("Unable to create event"));
+        }
+    }
+
+ 
