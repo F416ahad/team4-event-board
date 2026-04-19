b6c0527a0e9bafa49b4640228ea954b1f5508e7f
Huy Le <HuyLe32006@gmail.com>
2026-04-18 22:59:56 -0400

    feat: added error class to rsvp for cancelled event

--- src/rsvp/errors.ts
+++ src/rsvp/errors.ts
@@ -3,3 +3,7 @@
 export class EventNotFoundError extends Error {
   constructor() { super("Event not found"); }
 }
+
+export class EventCancelledError extends Error {
+  constructor() { super("Cannot RSVP to a cancelled event"); }
+}
