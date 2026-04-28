1a1ce1c3f17dd63aaf58f4d6774558c4bc11e35c
Huy Le <HuyLe32006@gmail.com>
2026-04-27 21:50:23 -0400

    feat: add create table for rsvp

--- prisma/migrations/20260427212451_init/migration.sql
+++ prisma/migrations/20260427212451_init/migration.sql
@@ -22,3 +22,15 @@ CREATE TABLE "Event" (
     CONSTRAINT "Event_createdByUserId_fkey" FOREIGN KEY ("createdByUserId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "Rsvp" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Rsvp_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Rsvp_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
