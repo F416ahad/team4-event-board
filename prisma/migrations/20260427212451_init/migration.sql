5f643eef89a6c17faa3c9c8c395f56ef91c0a74d
Huy Le <HuyLe32006@gmail.com>
2026-04-27 21:50:08 -0400

    feat: add create table for event

--- prisma/migrations/20260427212451_init/migration.sql
+++ prisma/migrations/20260427212451_init/migration.sql
@@ -9,3 +9,16 @@ CREATE TABLE "User" (
     "updatedAt" DATETIME NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "capacity" INTEGER,
+    "status" TEXT NOT NULL DEFAULT 'active',
+    "date" DATETIME NOT NULL,
+    "createdByUserId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Event_createdByUserId_fkey" FOREIGN KEY ("createdByUserId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
